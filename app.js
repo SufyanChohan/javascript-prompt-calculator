71f1fdc846acf0fd51243fd9b048e57793c07c6e
Sufyan Chohan <chohansufyan69@gmail.com>
2025-01-20 15:54:19 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+function add(){
+    let num1 = parseFloat(prompt("write 1st value"));
+    let num2 =parseFloat( prompt("write 2nd value"));
+    alert(num1+num2);
+}
+function sub(){
+    let num1 = parseFloat(prompt("write 1st value"));
+    let num2 =parseFloat( prompt("write 2nd value"));
+    alert(num1-num2);
+}
+function mult(){
+    let num1 = parseFloat(prompt("write 1st value"));
+    let num2 =parseFloat( prompt("write 2nd value"));
+    alert(num1*num2);
+}
+function divide(){
+    let num1 = parseFloat(prompt("write 1st value"));
+    let num2 =parseFloat( prompt("write 2nd value"));
+    alert(num1/num2);
+}
